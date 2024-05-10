1dc3b9bc377847b71d54e2877ac871ab089aa651
Изудинов Мухамед <m.izudinov@digitalms.ru>
2024-05-10 15:01:05 +0300

    remove unused files and data

--- apps/skl/src/app/app.component.spec.ts
+++ apps/skl/src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -15,11 +13,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Welcome skl');
-  });
-
-  it(`should have as title 'skl'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('skl');
   });
 });
